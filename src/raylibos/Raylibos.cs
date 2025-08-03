21c1450f49f08f1008ec738acfffe33443fa623b
Никита Иванченко <nikita.ivanchenko@vkteam.ru>
2025-08-03 19:44:29 +0300

    init

---
+++ src/raylibos/Raylibos.cs
@@ -0,0 +1,47 @@
+﻿using OneScript.Contexts;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.Machine;
+using Raylib_cs;
+
+namespace raylibos;
+
+[ContextClass("Рейлиб", "Raylib")]
+public class Raylibos : AutoContext<Raylibos>
+{
+
+    [ScriptConstructor]
+    public static Raylibos Constructor()
+    {
+        return new Raylibos();
+    }
+
+    [ContextMethod("ИнициализацияОкна", "InitWindow")]
+    public void InitWindow(string title, int width, int height)
+    {
+        Raylib.InitWindow(width, height, title);
+    }
+
+    [ContextMethod("ОкноДолжноЗакрыться", "WindowShouldClose")]
+    public bool WindowShouldClose()
+    {
+        return Raylib.WindowShouldClose();
+    }
+
+    [ContextMethod("НачатьОтрисовку", "BeginDrawing")]
+    public void BeginDrawing()
+    {
+        Raylib.BeginDrawing();
+    }
+
+    [ContextMethod("ЗакончитьОтрисовку", "EndDrawing")]
+    public void EndDrawing()
+    {
+        Raylib.EndDrawing();
+    }
+    
+    [ContextMethod("ЗакрытьОкно", "CloseWindow")]
+    public void CloseWindow()
+    {
+        Raylib.CloseWindow();
+    }
+}
